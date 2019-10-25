70996075b7dad10ff9b9e4f06b9bddc86ac5ae3a
Jake Morrison <jake@cogini.com>
2019-10-25 12:08:52 +0800

    Add static analysis tools

--- mix.exs
+++ mix.exs
@@ -14,7 +14,9 @@ defmodule ConfluentSchemaRegistry.MixProject do
       source_url: "https://github.com/cogini/confluent_schema_registry",
       homepage_url: "https://github.com/cogini/confluent_schema_registry",
       deps: deps(),
-      docs: docs()
+      docs: docs(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
     ]
   end
 
@@ -40,6 +42,8 @@ defmodule ConfluentSchemaRegistry.MixProject do
       {:jason, "~> 1.0"},
       {:hackney, "~> 1.14", only: [:dev, :test]},
       {:dialyxir, "~> 0.5.1", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.12.0", only: [:dev, :test], runtime: false},
       {:telemetry, "~> 0.4.0"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
