f78ca595aef327f53ee9255b0d8e6293394ddcab
Dave Lucia <davelucianyc@gmail.com>
2022-02-22 20:41:11 -0500

    v0.1.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ConfluentSchemaRegistry.MixProject do
   def project do
     [
       app: :confluent_schema_registry,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
