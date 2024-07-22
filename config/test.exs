a86a6979985e6de506ba8595e3d03dc85f79dae7
Jake Morrison <jake@cogini.com>
2024-07-22 16:42:37 -0500

    Configure junit_formatter

--- config/test.exs
+++ config/test.exs
@@ -5,3 +5,11 @@ import Config
 #   level: :debug,
 #   backends: [:console],
 #   compile_time_purge_level: :debug
+
+config :junit_formatter,
+  report_dir: "#{Mix.Project.build_path()}/junit-reports",
+  automatic_create_dir?: true,
+  print_report_file: true,
+  # prepend_project_name?: true,
+  include_filename?: true,
+  include_file_line?: true
