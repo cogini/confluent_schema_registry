306b2c78122ac8f890e140b39969a117dac0dfe5
Jake Morrison <jake@cogini.com>
2024-07-22 16:23:22 -0500

    Do standard things

--- mix.exs
+++ mix.exs
@@ -2,38 +2,39 @@ defmodule ConfluentSchemaRegistry.MixProject do
   use Mix.Project
 
   @github "https://github.com/cogini/confluent_schema_registry"
+  @version "0.1.1"
 
   def project do
     [
       app: :confluent_schema_registry,
-      version: "0.1.1",
+      version: @version,
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      description: description(),
-      package: package(),
-      source_url: @github,
-      homepage_url: @github,
-      docs: docs(),
+      aliases: aliases(),
+      dialyzer: [
+        plt_add_apps: [:mix, :eex]
+        # flags: ["-Wunmatched_returns", :error_handling, :race_conditions, :underspecs]
+      ],
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
         coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
-        "coveralls.html": :test
+        "coveralls.html": :test,
+        "coveralls.lcov": :test,
+        quality: :test,
+        "quality.ci": :test
       ],
-      dialyzer: [
-        # plt_add_apps: [:erlavro, :tesla],
-        # plt_add_deps: true,
-        # flags: ["-Werror_handling", "-Wrace_conditions"],
-        flags: ["-Wunmatched_returns", :error_handling, :race_conditions, :underspecs]
-        # ignore_warnings: "dialyzer.ignore-warnings"
-      ],
+      description: description(),
+      package: package(),
+      source_url: @github,
+      homepage_url: @github,
+      docs: docs(),
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger] ++ extra_applications(Mix.env())
@@ -67,18 +68,58 @@ defmodule ConfluentSchemaRegistry.MixProject do
 
   defp package do
     [
+      description: description(),
       maintainers: ["Jake Morrison"],
       licenses: ["Apache-2.0"],
-      links: %{"GitHub" => @github}
+      links: %{
+        "GitHub" => @github,
+        "Changelog" => "#{@github}/blob/#{@version}/CHANGELOG.md##{String.replace(@version, ".", "")}"
+      }
     ]
   end
 
   defp docs do
     [
+      main: "readme",
       source_url: @github,
-      extras: ["README.md", "CHANGELOG.md"],
+      source_ref: @version,
+      extras: [
+        "README.md",
+        "CHANGELOG.md": [title: "Changelog"],
+        "LICENSE.md": [title: "License (Apache-2.0)"],
+        "CONTRIBUTING.md": [title: "Contributing"],
+        "CODE_OF_CONDUCT.md": [title: "Code of Conduct"]
+      ],
       # api_reference: false,
       source_url_pattern: "#{@github}/blob/master/%{path}#L%{line}"
+    ]
+  end
+
+  defp aliases do
+    [
+      setup: ["deps.get"],
+      quality: [
+        "test",
+        "format --check-formatted",
+        # "credo",
+        "credo --mute-exit-status",
+        # mix deps.clean --unlock --unused
+        "deps.unlock --check-unused",
+        # mix deps.update
+        # "hex.outdated",
+        # "hex.audit",
+        "deps.audit",
+        "dialyzer --quiet-with-result"
+      ],
+      "quality.ci": [
+        "format --check-formatted",
+        "deps.unlock --check-unused",
+        # "hex.outdated",
+        "hex.audit",
+        "deps.audit",
+        "credo",
+        "dialyzer --quiet-with-result"
+      ]
     ]
   end
 end
